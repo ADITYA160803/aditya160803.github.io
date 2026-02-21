97127f7214a6fab1fde92efae1b4b801f2228557
ADITYA160803 <adityabadarpuria@gmail.com>
2026-02-21 16:07:02 +0530

    Updates

--- assets/Navbar-DhmCa1bX.js
+++ assets/Navbar-DhmCa1bX.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-B56rqgMg.js";import{g as yt,S as St}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-BA2dRWSj.js";import{g as yt,S as St}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
